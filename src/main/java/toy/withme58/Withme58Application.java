1f7e8e951030d51cc8cd0621092b70d47fd0af4e
Sumin <new.sumyang@gmail.com>
2024-07-03 13:45:26 +0900

    init

---
+++ src/main/java/toy/withme58/Withme58Application.java
@@ -0,0 +1,13 @@
+package toy.withme58;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Withme58Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Withme58Application.class, args);
+	}
+
+}
---
+++ src/test/java/toy/withme58/Withme58ApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.withme58;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Withme58ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
