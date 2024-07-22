2c41d1733ca817c45dd984c0143c14e99dc03c47
Sumin <new.sumyang@gmail.com>
2024-07-22 22:46:29 +0900

    [modify] : RequestMapping 어노테이션 사용

--- src/main/java/toy/withme58/api/home/controller/HomeController.java
+++ src/main/java/toy/withme58/api/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package toy.withme58.api.home.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import toy.withme58.api.common.annotation.MemberSession;
 import toy.withme58.api.common.api.Api;
@@ -10,12 +11,13 @@ import toy.withme58.api.home.dto.response.HomeResponse;
 import toy.withme58.api.member.dto.Member;
 
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class HomeController {
 
     private final HomeBusiness homeBusiness;
 
-    @GetMapping("/api")
+    @GetMapping
     public Api<HomeResponse> home(@MemberSession Member member) {
         HomeResponse homeResponse = homeBusiness.homeResponse(member.getId());
         return Api.OK(homeResponse);
