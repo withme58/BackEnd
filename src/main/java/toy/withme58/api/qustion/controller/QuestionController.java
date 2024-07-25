d783e98aec3d8a78341dbc46f60fb04dd63ed6a6
Sumin <new.sumyang@gmail.com>
2024-07-25 21:36:51 +0900

    [feat] : 질문조회 controller 생성

---
+++ src/main/java/toy/withme58/api/qustion/controller/QuestionController.java
@@ -0,0 +1,13 @@
+package toy.withme58.api.qustion.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/question")
+@RestController
+public class QuestionController {
+
+    
+}
