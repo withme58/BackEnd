2bd88f14cf7545ae629f0ed10d57d14882c7e84e
PlusUltraCode <kkd06144@naver.com>
2024-08-02 19:46:15 +0900

    feat : swagger token 인증 버튼 구현 완료

--- src/main/java/toy/withme58/api/common/config/swagger/SwaggerConfig.java
+++ src/main/java/toy/withme58/api/common/config/swagger/SwaggerConfig.java
@@ -2,6 +2,10 @@ package toy.withme58.api.common.config.swagger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +17,20 @@ public class SwaggerConfig {
     public ModelResolver modelResolver(ObjectMapper objectMapper){
         return new ModelResolver(objectMapper);
 
+    }
+
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("authorization-token");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
     }
 }
