1d931f702bd180f563ac831ed6c5eabb595584c1
Sumin <new.sumyang@gmail.com>
2024-08-04 22:35:40 +0900

    [modify] : CorsConfig 파일 정리

--- src/main/java/toy/withme58/api/common/config/CorsConfig.java
+++ src/main/java/toy/withme58/api/common/config/CorsConfig.java
@@ -14,16 +14,15 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("http://localhost");
-        config.addAllowedOrigin("http://52.78.139.165:8080");
-        config.addAllowedOrigin("http://52.78.139.165");
-        config.addAllowedOrigin("https://52.78.139.165:8080");
-        config.addAllowedOrigin("https://52.78.139.165");
         config.addAllowedOrigin("https://peephole.co.kr");
         config.addAllowedOrigin("https://www.peephole.co.kr");
+        config.addAllowedOrigin("https://peephole-inks-projects-552d16c4.vercel.app");
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
         config.addExposedHeader("Authorization");
         config.addExposedHeader("refreshToken");
 
