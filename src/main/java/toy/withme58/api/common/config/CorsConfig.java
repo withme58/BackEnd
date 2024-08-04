aa70d8b6fd947593cf0ef16fe7fe0a4b908cff00
Sumin <new.sumyang@gmail.com>
2024-08-04 13:46:54 +0900

    [modify]: Cors Config https까지 허용하도록 수정

--- src/main/java/toy/withme58/api/common/config/CorsConfig.java
+++ src/main/java/toy/withme58/api/common/config/CorsConfig.java
@@ -16,8 +16,10 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost");
-        config.addAllowedOrigin("http://43.200.67.235:8080");
-        config.addAllowedOrigin("http://43.200.67.235");
+        config.addAllowedOrigin("http://52.78.139.165:8080");
+        config.addAllowedOrigin("http://52.78.139.165");
+        config.addAllowedOrigin("https://52.78.139.165:8080");
+        config.addAllowedOrigin("https://52.78.139.165");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
