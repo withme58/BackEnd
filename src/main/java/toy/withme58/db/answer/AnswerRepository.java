e8a4dfce688c684c755b84db10b186c0c191f986
Sumin <new.sumyang@gmail.com>
2024-08-06 09:14:59 +0900

    [modify] : answerRepository쿼리문 수정

--- src/main/java/toy/withme58/db/answer/AnswerRepository.java
+++ src/main/java/toy/withme58/db/answer/AnswerRepository.java
@@ -39,6 +39,6 @@ public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
     @Query("select count(a) from AnswerEntity a where a.receiverId = :receiverId and a.content is not null")
     Long countByReceiverIdAndContentIsNotNull(@Param("receiverId") Long receiverId);
 
-    @Query("select a from AnswerEntity a where a.senderId = :senderId And DATE(a.createdAt) = :date Order By a.id desc")
+    @Query("select a from AnswerEntity a where a.senderId = :senderId And FUNCTION('DATE', a.createdAt) = :date Order By a.id desc")
     Optional<AnswerEntity> findFirstBySenderIdAndCreatedAtOrderByIdDesc(@Param("senderId")Long senderId,@Param("date") LocalDate date);
 }
